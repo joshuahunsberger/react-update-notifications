91c091452c5d4858bcf4b3fe339d91e824de2839
Joshua Hunsberger <joshua@hunsberger.co>
2025-11-08 16:35:56 -0500

    Add VitePWA plugin and setup for GH pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+const REPO_NAME = "react-update-notifications";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: `/${REPO_NAME}/`,
+  plugins: [
+    react(),
+    VitePWA({
+      // 2. Use 'prompt' strategy
+      registerType: "prompt",
+
+      // 3. No manifest needed for this demo
+      manifest: false,
+
+      // 4. Don't run in dev mode
+      devOptions: {
+        enabled: false,
+      },
+
+      workbox: {
+        // 5. This ensures all assets are included
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+    }),
+  ],
+});
