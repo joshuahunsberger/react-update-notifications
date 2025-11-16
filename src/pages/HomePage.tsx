b47d164a033a4d2758c32e6753e9d79b226c5ae3
Joshua Hunsberger <joshua@hunsberger.co>
2025-11-15 22:42:39 -0500

    Update home page to trigger a reload

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,7 @@
 function HomePage() {
   // This is the text we will change between v1 and v2
-  const version: string = "v1.0"; // Change to 'v2.0' for the update
-  const message: string = "This is the first deployed version."; // Change this too
-
+  const version: string = "v2.0";
+  const message: string = "This is the second deployed version.";
   return (
     <header className="App-header">
       <h1>Service Worker Demo - {version}</h1>
